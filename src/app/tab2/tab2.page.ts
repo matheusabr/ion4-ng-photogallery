03e7d00aeed1059aa215ca169c66796bbf072727
Matheus Abreu <matheusabreu.dev@gmail.com>
2019-08-28 22:37:45 +0100

    task(Gallery): take and show a picture

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
 
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  currentImage: any;
 
-  constructor() {}
+  constructor(private camera: Camera) { }
 
+  takePicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      correctOrientation: true
+    };
+
+    this.camera.getPicture(options)
+      .then((imageData) => {
+        // imageData is either a base64 encoded string or a file URI
+        // If it's base64 (DATA_URL):
+        this.currentImage = 'data:image/jpeg;base64,' + imageData;
+      }, (err) => {
+        // Handle error
+        console.error("Camera issue:" + err);
+      });
+  }
 }
